07243720bb1fcd7b860427348c54377cdf61146b
Cody Bumgardner <cody@uky.edu>
2019-07-08 12:24:51 -0400

    build it

--- src/main/java/core/HostApplication.java
+++ src/main/java/core/HostApplication.java
@@ -98,6 +98,7 @@ public class HostApplication
 
                         }
 
+
                         if(controllerBundle != null) {
                              controllerBundle.stop();
                         }
