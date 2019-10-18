1071ebdc03a8f24166a1b21b5f80ca8c0f687c44
Cody Bumgardner <cody@uky.edu>
2019-10-18 15:40:28 -0400

    woot

---
+++ package.bat
@@ -0,0 +1,2 @@
+
+CALL javapackager -deploy -native -outdir target -outfile CrescoAgent -srcdir target -srcfiles agent-1.0-SNAPSHOT.jar -appclass core.AgentEngine -name "CrescoAgent" -title "Cresco Agent"
