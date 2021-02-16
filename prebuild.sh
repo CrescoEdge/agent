73e83ec2235a7b4b523c36abc829b95029f59878
Cody Bumgardner <cody@uky.edu>
2021-02-16 11:23:50 -0500

    update version on script

--- prebuild.sh
+++ prebuild.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-rm src/main/resources/logger-1.0-SNAPSHOT.jar
-rm src/main/resources/core-1.0-SNAPSHOT.jar
-rm src/main/resources/library-1.0-SNAPSHOT.jar
-rm src/main/resources/controller-1.0-SNAPSHOT.jar
-rm src/main/resources/repo-1.0-SNAPSHOT.jar
-rm src/main/resources/sysinfo-1.0-SNAPSHOT.jar
-rm src/main/resources/dashboard-1.0-SNAPSHOT.jar
+rm src/main/resources/logger-1.1-SNAPSHOT.jar
+rm src/main/resources/core-1.1-SNAPSHOT.jar
+rm src/main/resources/library-1.1-SNAPSHOT.jar
+rm src/main/resources/controller-1.1-SNAPSHOT.jar
+rm src/main/resources/repo-1.1-SNAPSHOT.jar
+rm src/main/resources/sysinfo-1.1-SNAPSHOT.jar
+rm src/main/resources/dashboard-1.1-SNAPSHOT.jar
 
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:logger:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:core:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:library:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:controller:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:repo:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:sysinfo:1.0-SNAPSHOT
-mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:dashboard:1.0-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:logger:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:core:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:library:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:controller:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:repo:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:sysinfo:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:dashboard:1.1-SNAPSHOT
 
-cp ~/.m2/repository/io/cresco/logger/1.0-SNAPSHOT/logger-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/core/1.0-SNAPSHOT/core-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/library/1.0-SNAPSHOT/library-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/controller/1.0-SNAPSHOT/controller-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/repo/1.0-SNAPSHOT/repo-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/sysinfo/1.0-SNAPSHOT/sysinfo-1.0-SNAPSHOT.jar src/main/resources/
-cp ~/.m2/repository/io/cresco/dashboard/1.0-SNAPSHOT/dashboard-1.0-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/logger/1.1-SNAPSHOT/logger-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/core/1.1-SNAPSHOT/core-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/library/1.1-SNAPSHOT/library-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/controller/1.1-SNAPSHOT/controller-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/repo/1.1-SNAPSHOT/repo-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/sysinfo/1.1-SNAPSHOT/sysinfo-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/dashboard/1.1-SNAPSHOT/dashboard-1.1-SNAPSHOT.jar src/main/resources/
