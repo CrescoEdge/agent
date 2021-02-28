3a886c53dd368870d449d7edc98455df1130069d
codybum <cbumgardner@gmail.com>
2021-02-28 10:56:32 -0500

    seperate ws api

--- prebuild.bat
+++ prebuild.bat
@@ -6,6 +6,7 @@ CALL mvn org.apache.maven.plugins:maven-dependency-plugin:get -DrepoUrl=https://
 CALL mvn org.apache.maven.plugins:maven-dependency-plugin:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:repo:1.1-SNAPSHOT
 CALL mvn org.apache.maven.plugins:maven-dependency-plugin:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:sysinfo:1.1-SNAPSHOT
 CALL mvn org.apache.maven.plugins:maven-dependency-plugin:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:dashboard:1.1-SNAPSHOT
+CALL mvn org.apache.maven.plugins:maven-dependency-plugin:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:wsapi:1.1-SNAPSHOT
 
 copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\logger\1.1-SNAPSHOT\logger-1.1-SNAPSHOT.jar src\main\resources\
 copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\core\1.1-SNAPSHOT\core-1.1-SNAPSHOT.jar src\main\resources\
@@ -14,3 +15,4 @@ copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\controller\1.1-SNAPSHOT
 copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\repo\1.1-SNAPSHOT\repo-1.1-SNAPSHOT.jar src\main\resources\
 copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\sysinfo\1.1-SNAPSHOT\sysinfo-1.1-SNAPSHOT.jar src\main\resources\
 copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\dashboard\1.1-SNAPSHOT\dashboard-1.1-SNAPSHOT.jar src\main\resources\
+copy /Y %systemdrive%%homepath%\.m2\repository\io\cresco\wsapi\1.1-SNAPSHOT\wsapi-1.1-SNAPSHOT.jar src\main\resources\
--- prebuild.sh
+++ prebuild.sh
@@ -7,6 +7,7 @@ rm src/main/resources/controller-1.1-SNAPSHOT.jar
 rm src/main/resources/repo-1.1-SNAPSHOT.jar
 rm src/main/resources/sysinfo-1.1-SNAPSHOT.jar
 rm src/main/resources/dashboard-1.1-SNAPSHOT.jar
+rm src/main/resources/wsapi-1.1-SNAPSHOT.jar
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:logger:1.1-SNAPSHOT
 mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:core:1.1-SNAPSHOT
@@ -15,6 +16,7 @@ mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -Dre
 mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:repo:1.1-SNAPSHOT
 mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:sysinfo:1.1-SNAPSHOT
 mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:dashboard:1.1-SNAPSHOT
+mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dtransitive=false -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=io.cresco:wsapi:1.1-SNAPSHOT
 
 cp ~/.m2/repository/io/cresco/logger/1.1-SNAPSHOT/logger-1.1-SNAPSHOT.jar src/main/resources/
 cp ~/.m2/repository/io/cresco/core/1.1-SNAPSHOT/core-1.1-SNAPSHOT.jar src/main/resources/
@@ -23,3 +25,4 @@ cp ~/.m2/repository/io/cresco/controller/1.1-SNAPSHOT/controller-1.1-SNAPSHOT.ja
 cp ~/.m2/repository/io/cresco/repo/1.1-SNAPSHOT/repo-1.1-SNAPSHOT.jar src/main/resources/
 cp ~/.m2/repository/io/cresco/sysinfo/1.1-SNAPSHOT/sysinfo-1.1-SNAPSHOT.jar src/main/resources/
 cp ~/.m2/repository/io/cresco/dashboard/1.1-SNAPSHOT/dashboard-1.1-SNAPSHOT.jar src/main/resources/
+cp ~/.m2/repository/io/cresco/wsapi/1.1-SNAPSHOT/wsapi-1.1-SNAPSHOT.jar src/main/resources/
