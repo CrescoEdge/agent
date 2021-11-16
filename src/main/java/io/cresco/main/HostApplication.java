aa19ab3c117a2bdbd1a6ac18539007379e5315ab
codybum <cbumgardner@gmail.com>
2021-11-16 13:35:35 -0500

    bam

--- src/main/java/io/cresco/main/HostApplication.java
+++ src/main/java/io/cresco/main/HostApplication.java
@@ -424,6 +424,7 @@ public class HostApplication
                 }
             }
 
+
             //create set directory if it does not exist
             String cresco_data_directory = System.getProperty("cresco_data_location");
             if(cresco_data_directory != null) {
